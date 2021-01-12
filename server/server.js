c4b9e5aa9a96380e6ec7f82ba90521ca50e1603a
Mason Pott <masoncpott@gmail.com>
2021-01-12 17:07:32 -0600

    webpack config, server running, rendered 'hello world' to DOM

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-//express server here
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.static('./public/dist'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+
+app.listen(6001, function () {
+  console.log('listening on port 6001!');
+})

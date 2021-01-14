7014e623d20400f2ce21388dd7675d6754d2c91c
Mason Pott <masoncpott@gmail.com>
2021-01-13 22:54:15 -0600

    able to generate a mix of full and partial records

--- database/fakeData/overviewDummyData.js
+++ database/fakeData/overviewDummyData.js
@@ -24,27 +24,59 @@ oneRecord = {
 }
 */
 
+const randomNumberGenerator = (num) => {
+  return faker.random.number(num);
+}
+
 const generateOneRecord = () => {
   const oneRecord = {
-    product_description:  faker.lorem.sentence(20),
+    product_description:  faker.lorem.sentence(randomNumberGenerator(35)),
     feature1_title:       faker.lorem.word(),
-    feature1_description: faker.lorem.words(7),
+    feature1_description: faker.lorem.words(randomNumberGenerator(15)),
     feature2_title:       faker.lorem.word(),
-    feature2_description: faker.lorem.words(12),
+    feature2_description: faker.lorem.words(randomNumberGenerator(10)),
     feature3_title:       faker.lorem.word(),
-    feature3_description: faker.lorem.words(),
+    feature3_description: faker.lorem.words(randomNumberGenerator(7)),
     feature4_title:       faker.lorem.word(),
-    feature4_description: faker.lorem.words(),
+    feature4_description: faker.lorem.words(randomNumberGenerator(15)),
     feature5_title:       faker.lorem.word(),
-    feature5_description: faker.lorem.words(),
+    feature5_description: faker.lorem.words(randomNumberGenerator(10)),
     feature6_title:       faker.lorem.word(),
-    feature6_description: faker.lorem.words(),
+    feature6_description: faker.lorem.words(randomNumberGenerator(15)),
     weight:               faker.random.number(100).toString()
   };
 
   return oneRecord;
-}
+};
 
+const generatePartialRecord = () => {
+  const partialRecord = {
+    product_description:  faker.lorem.sentence(randomNumberGenerator(35)),
+    feature1_title:       faker.lorem.word(),
+    feature1_description: faker.lorem.words(randomNumberGenerator(15)),
+    feature2_title:       faker.lorem.word(),
+    feature2_description: faker.lorem.words(randomNumberGenerator(10)),
+    feature3_title:       faker.lorem.word(),
+    feature3_description: faker.lorem.words(randomNumberGenerator(7)),
+    feature4_title:       null,
+    feature4_description: null,
+    feature5_title:       null,
+    feature5_description: null,
+    feature6_title:       null,
+    feature6_description: null,
+    weight:               faker.random.number(100).toString()
+  }
+  return partialRecord;
+};
 
-const testRecord = generateOneRecord();
-console.log('ONE RECORD: ', testRecord);
+const fiveRecords = [];
+
+for(i = 0; i < 6; i++) {
+  if(i % 2 === 0) {
+    fiveRecords.push(generateOneRecord());
+  } else {
+    fiveRecords.push(generatePartialRecord());
+  }
+};
+
+console.log(fiveRecords);

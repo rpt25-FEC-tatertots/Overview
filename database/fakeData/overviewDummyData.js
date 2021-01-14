a5ccb88bffad6604ee7008c6131406d61f8bf240
Mason Pott <masoncpott@gmail.com>
2021-01-13 22:40:25 -0600

    able to generate one fake record for Overview using faker.js

---
+++ database/fakeData/overviewDummyData.js
@@ -0,0 +1,50 @@
+const faker = require('faker');
+
+//generate 100 record for overview table
+
+//add all 100 to database
+
+/*
+oneRecord = {
+  product_id: NUMBER,
+  product_description: STRING,
+  feature1_title: STRING,
+  feature1_description: STRING,
+  feature2_title: STRING,
+  feature2_description: STRING,
+  feature3_title: STRING,
+  feature3_description: STRING,
+  feature4_title: STRING,
+  feature4_description: STRING,
+  feature5_title: STRING,
+  feature5_description: STRING,
+  feature6_title: STRING,
+  feature6_description: STRING,
+  weight: STRING
+}
+*/
+
+const generateOneRecord = () => {
+  const oneRecord = {
+    product_description:  faker.lorem.sentence(20),
+    feature1_title:       faker.lorem.word(),
+    feature1_description: faker.lorem.words(7),
+    feature2_title:       faker.lorem.word(),
+    feature2_description: faker.lorem.words(12),
+    feature3_title:       faker.lorem.word(),
+    feature3_description: faker.lorem.words(),
+    feature4_title:       faker.lorem.word(),
+    feature4_description: faker.lorem.words(),
+    feature5_title:       faker.lorem.word(),
+    feature5_description: faker.lorem.words(),
+    feature6_title:       faker.lorem.word(),
+    feature6_description: faker.lorem.words(),
+    weight:               faker.random.number(100).toString()
+  };
+
+  return oneRecord;
+}
+
+
+const testRecord = generateOneRecord();
+console.log('ONE RECORD: ', testRecord);

0b672f6a2f1de0851ebfbdca58df8416faa55e39
Mason Pott <masoncpott@gmail.com>
2021-01-26 10:55:09 -0600

    made notes to plan out next steps. No major changes in this commit. Everything works well so far.

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// create style that can be applied to feature title text
+
+// create style that can be applies to feature description text
+
+// create container with flexbox to hold everything
+
+// for each row, create a container with two columns
+
+// display all rows in a column
+
 class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +20,10 @@ class ProductDetails extends React.Component {
 
   render() {
     return (
-      <div>HELLO WORLD</div>
+      <div>
+        <div>Feature Title</div>
+        <div>Feature Description</div>
+      </div>
     );
   }
 }

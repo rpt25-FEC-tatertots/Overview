396faca721a86138d5cd232873a942023e28aec8
Mason Pott <masoncpott@gmail.com>
2021-01-26 14:24:01 -0600

    Need to refactor the component tree I think. Cutting a new branch.

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FeatureContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 // create style that can be applied to feature title text
+const FeatureHeader = styled.h2`
+  font-family: Nunito Sans;
+  font-weight: bold;
+  flex-basis: 25.390625%;
+`;
 
 // create style that can be applies to feature description text
+const FeatureDescription = styled.div`
+  font-family: Nunito Sans;
+  font-weight: 400;
+`;
 
 // create container with flexbox to hold everything
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 50%;
+`;
 
 // for each row, create a container with two columns
+const DetailsRow = styled.div`
+  display: flex;
+`;
 
 // display all rows in a column
 
@@ -20,10 +42,17 @@ class ProductDetails extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>Feature Title</div>
-        <div>Feature Description</div>
-      </div>
+      <FeatureContainer>
+        <FeatureHeader>Features</FeatureHeader>
+        <DetailsContainer>
+          <DetailsRow>
+            <div>Feature Title</div>
+            <div>Feature Description</div>
+            <div>Feature Title</div>
+            <div>Feature Description</div>
+          </DetailsRow>
+        </DetailsContainer>
+      </FeatureContainer>
     );
   }
 }

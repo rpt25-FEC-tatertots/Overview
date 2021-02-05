126bce65852b41dec606ef8f2526b36ea66bb84b
Mason Pott <masoncpott@gmail.com>
2021-02-05 14:27:29 -0600

    able to click on an icon and dynamically display the title and description of it

--- client/src/components/IconComponent.jsx
+++ client/src/components/IconComponent.jsx
@@ -22,25 +22,36 @@ class IconComponent extends React.Component {
     this.state = {
       currentIndex: 0,
     };
+    this.updateSelectedIcon = this.updateSelectedIcon.bind(this);
+  }
+
+  updateSelectedIcon(i) {
+    console.log('CLICKED', i);
+    this.setState({ currentIndex: i });
   }
 
   render() {
     const { icons } = this.props;
+    console.log(icons)
     if (icons && icons.length > 0) {
       const displayIcons = icons.map((icon, index) => {
         return (
-          <StyledIconButton key={icon.icon_title}>
-            <svg fill="white" width="50" height="50" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
-              <path d={icon.icon_svg} />
-            </svg>
-          </StyledIconButton>
+            <StyledIconButton key={icon.icon_title} onClick={() => this.updateSelectedIcon(index)}>
+              <svg fill="white" width="50" height="50" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+                <path d={icon.icon_svg} />
+              </svg>
+            </StyledIconButton>
         );
       });
 
       return (
-        <StyledIconContainer>
-          {displayIcons}
-        </StyledIconContainer>
+        <>
+          <StyledIconContainer>
+            {displayIcons}
+          </StyledIconContainer>
+          <div>{icons[this.state.currentIndex].icon_title}</div>
+          <div>{icons[this.state.currentIndex].icon_description}</div>
+        </>
       );
     }
 

80bd7515b31d294bf34985c183db25867596a531
Mason Pott <masoncpott@gmail.com>
2021-02-15 17:52:22 -0600

    another console.log

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.get('/overview/:product_id', async (req, res) => {
   const data = {};
   try {
     const overviewInfo = await db.overview.findByPk(productNum, { include: [db.icons] });
+    console.log('OVERVIEW INFO: ', overviewInfo);
     const materialsInfo = await axios.get(`http://ec2-3-140-250-26.us-east-2.compute.amazonaws.com:5002/materials/${productNum}`);
     data.overviewInfo = overviewInfo.dataValues;
     data.materialsInfo = materialsInfo.data;
